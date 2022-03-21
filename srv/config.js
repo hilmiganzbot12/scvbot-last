6657744030f9e40d372ffd61e488396686b938c3
Clicknetcafe <rama4shared@gmail.com>
2022-03-21 16:35:28 +0800

    Update config.js

--- srv/config.js
+++ srv/config.js
@@ -1,12 +1,3 @@
-//NOTE :
-//All case using lol api, if seems not working, change the key
-
-//commands that use imggb key :
-// - readqr, readbarcode, ceknsfw, cekusia, ocr, wait, wmit, trigger
-// - all command in editormenu
-
-// YEP THATS ALL
-
 {
 "Lolkey": "260153ea4dfab79e14a9c7ae",
 "ImgbbKey": "7b2fde1f55b9a594d5db403f0a0c2712"
